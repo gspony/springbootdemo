2319f9e1aab2e50ce7817cfa1b8a1253ec278585
gspony <cn.maweijun@gmail.com>
2019-06-27 16:57:19 +0800

    spring boot demo 2.16

--- src/main/java/com/springboot/demo/DemoApplication.java
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description 
@@ -26,6 +30,19 @@ public class DemoApplication {
     String first() {
         return "This is first";
     }
+    @RequestMapping("/rest")
+    @ResponseBody
+    public Map<String, Object> rest() {
+
+        Map<String, Object> data = new HashMap<String, Object>();
+
+        data.put("1", "A");
+        data.put("2", 2);
+
+        return data;
+
+    }
+
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
